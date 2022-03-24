7c46cf004f4e49b93b8dc4d1ed4387340efc95fe
Beau Hibbert <beauhibbert789@gmail.com>
2022-03-23 18:06:57 -0700

    everything except tests

---
+++ src/models/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const clothesSchema = require('./clothes.schema.js');
+const foodSchema = require('./food.schema.js');
+
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? 'sqlite::memory'
+  : process.env.DATABASE_URL || 'postgresql://localhost:5432/api-app';
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+const ClothesModel = clothesSchema(sequelize, DataTypes);
+const FoodModel = foodSchema(sequelize, DataTypes);
+
+module.exports = {
+  sequelize,
+  ClothesModel,
+  FoodModel,
+};
